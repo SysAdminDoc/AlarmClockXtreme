b8a19a95ce8dddded7c65ee906d11a36e1ccbaca
Matthew Parker <matt_parker@outlook.com>
2026-05-13 12:02:49 -0400

    v1.9.3: recover exact alarm permission grants

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 import java.util.Properties
 import java.io.FileInputStream
 
-// AlarmClockXtreme v1.9.2
+// AlarmClockXtreme v1.9.3
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -18,8 +18,8 @@ android {
         applicationId = "com.sysadmindoc.alarmclock"
         minSdk = 26
         targetSdk = 35
-        versionCode = 39
-        versionName = "1.9.2"
+        versionCode = 40
+        versionName = "1.9.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
